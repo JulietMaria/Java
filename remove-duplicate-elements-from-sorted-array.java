8cfbe414b3a696d3bd92b2da1435bce793117dc8
JJM <45478187+JulietMaria@users.noreply.github.com>
2024-09-17 07:39:47 +0530

    Create remove-duplicate-elements-from-sorted-array.java

---
+++ remove-duplicate-elements-from-sorted-array.java
@@ -0,0 +1,13 @@
+//https://www.geeksforgeeks.org/problems/remove-duplicate-elements-from-sorted-array/1
+class Solution {
+    public int remove_duplicate(List<Integer> arr) {
+        int i = 0;
+        for (int j = 1; j < arr.size(); j++) {
+            if (!arr.get(j).equals(arr.get(i))) {
+                i++;
+                arr.set(i,arr.get(j));
+            }
+        }
+        return i + 1;
+    }
+}
